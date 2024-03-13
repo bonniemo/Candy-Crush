92a12b7ffd8df00e299a7852d35ebd90b894c677
Bonnie <moller.bon@gmail.com>
2024-03-13 11:48:07 +0100

    type alias added

--- src/Partials/CandyCrush.tsx
+++ src/Partials/CandyCrush.tsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 
+type Color = "green" | "blue" | "yellow"| "purple" | "hotpink";
+
 const width = 8;
 
-const candyColors = ["green", "blue", "yellow", "purple", "hotpink"];
+const candyColors: Color[] = ["green", "blue", "yellow", "purple", "hotpink"];
 
 const CandyCrush = () => {
-  const [colors, setColors] = useState([""]);
+  const [colors, setColors] = useState<Color[]>([]);
 
   const CreateBoard = () => {
-    const colorArray = [];
+    const colorArray: Color[] = [];
 
     for (let i = 0; i < width * width; i++) {
-      const candyColor =
+      const candyColor: Color =
         candyColors[Math.floor(Math.random() * candyColors.length)];
       colorArray.push(candyColor);
     }
@@ -27,7 +29,7 @@ const CandyCrush = () => {
     <>
       <h1>CandyCrush</h1>
       <section className="game-board">
-        {colors.map((color, index) => (
+        {colors.map((color: Color, index) => (
           <img key={index} 
           style={{backgroundColor: color}}
           ></img>
